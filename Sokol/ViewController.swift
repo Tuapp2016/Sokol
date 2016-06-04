ac567db52fb5f07f45be69333325d18427e05593
Andres Gutierrez <agutierrezt@unal.edu.co>
2016-06-03 21:21:19 -0500

    Set up

---
+++ Sokol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sokol
+//
+//  Created by Andres Rene Gutierrez on 03/06/2016.
+//  Copyright © 2016 Andres Rene Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
